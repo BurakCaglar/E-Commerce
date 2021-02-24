5af0a20245637a80b400555da6f728220ca507e9
BurakCaglar <burakcaglar@yandex.com>
2021-02-25 02:33:25 +0300

    responsive contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import img from "../assets/contact-background.svg";
+import img from "../assets/contact-background.jpg";
 import { ButtonV2 } from "./Buttons/Button";
 const Contact = () => {
   return (
     <Wrapper>
-      <div className="container-center">
+      <div className="container container-center">
         <div className="background">
           <div className="content">
             <h3>
@@ -28,8 +28,9 @@ const Contact = () => {
 };
 const Wrapper = styled.section`
   .background {
-    /* background-image: url(${img}); */
-    position: relative;
+    background-image: url(${img});
+    background-position: center;
+    box-shadow: inset 0 0 0 1000px rgba(0, 0, 0, 0.2);
     height: 24.2rem;
     display: flex;
     justify-content: center;
@@ -39,23 +40,45 @@ const Wrapper = styled.section`
     margin: auto;
     display: grid;
     grid-template-columns: 1fr 1fr;
-
+    grid-gap: 13rem;
     align-items: center;
+
+    @media only screen and (max-width: 1000px) {
+      display: grid;
+      grid-gap: 0;
+      grid-template-columns: auto;
+    }
 
     h3 {
       font-size: 2.7rem;
       font-weight: 300;
       color: var(--white);
+      @media only screen and (max-width: 1000px) {
+        font-size: 1.7rem;
+        text-align: center;
+      }
     }
 
     .contact-form {
+      @media only screen and (max-width: 1000px) {
+        text-align: center;
+      }
       input {
         width: 30rem;
         height: 4.3rem;
         border-radius: 5rem;
         border: none;
+        margin-right: 3rem;
+        @media only screen and (max-width: 1000px) {
+          width: 15rem;
+          height: 3.3rem;
+          margin: 1rem 0 0 0;
+        }
         ::placeholder {
           padding-left: 3.5rem;
+          @media only screen and (max-width: 1000px) {
+            padding-left: 2rem;
+          }
         }
       }
 
