9fbf8fc00046c9bf075b12a81c38129c4737a5c6
BurakCaglar <burakcaglar@yandex.com>
2021-02-24 22:46:44 +0300

    brand component added

---
+++ src/components/Helpers/Brand.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import logo from "../../assets/logo.svg";
+
+const Brand = () => {
+  return (
+    <Wrapper>
+      <Link to="/" className="brand2">
+        <img src={logo} alt="logo" />
+        <p>E-Shop</p>
+      </Link>
+    </Wrapper>
+  );
+};
+
+export default Brand;
+
+const Wrapper = styled.div`
+  .brand2 {
+    display: flex;
+    align-items: center;
+
+    p {
+      font-size: 1.5rem;
+      font-weight: 300;
+    }
+
+    img {
+      width: 4.5rem;
+      height: 3.5rem;
+      margin-right: 2rem;
+    }
+  }
+`;
