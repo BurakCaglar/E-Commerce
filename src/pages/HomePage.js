9743011813c1aa604de6c37c2dad9af13b3dad53
BurakCaglar <burakcaglar@yandex.com>
2021-02-25 20:40:58 +0300

    navbar styled for homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { SampleProducts, TopSlider, WhyChoose, Contact } from "../components";
+import NavbarV2 from "../components/Navbar&Sidebar/NavbarV2";
+import SliderFooter from "../components/SliderFooter";
 const HomePage = () => {
   return (
     <main>
+      <NavbarV2 />
+      <SliderFooter />
       <TopSlider />
       <SampleProducts />
       <WhyChoose />
