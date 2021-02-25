9ed0b77c82ee597cca4243a29a5af9d7d3a560dc
BurakCaglar <burakcaglar@yandex.com>
2021-02-25 03:53:08 +0300

    hr added to bottom of the navbar

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -4,21 +4,26 @@ import { Link } from "react-router-dom";
 import { AiOutlineHome } from "react-icons/ai";
 const Breadcrumb = ({ title, singleProduct }) => {
   return (
-    <Wrapper>
-      <div className="container-center">
-        <Link to="/">
-          <AiOutlineHome />
-        </Link>
-        {singleProduct ? <Link to="/products">/ Products</Link> : null}/ {title}
-      </div>
-    </Wrapper>
+    <>
+      <hr style={{ marginTop: "2rem" }} />
+      <Wrapper>
+        <div className="container-center">
+          <Link to="/">
+            <AiOutlineHome />
+          </Link>
+          {singleProduct ? <Link to="/products">/ Products</Link> : null}/{" "}
+          {title}
+        </div>
+      </Wrapper>
+    </>
   );
 };
 
 const Wrapper = styled.section`
   display: flex;
   color: var(--grey);
-  font-size: 1.3rem;
+  font-size: 1.5rem;
+  margin-top: 2rem;
   a {
     color: var(--grey);
     padding: 0.5rem;
