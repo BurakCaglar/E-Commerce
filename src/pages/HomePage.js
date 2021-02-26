5803b0788e6db544b686fa0bd4694ac12b3052ad
BurakCaglar <burakcaglar@yandex.com>
2021-02-26 03:49:17 +0300

    slider headline added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,11 +2,14 @@ import React from "react";
 import { SampleProducts, TopSlider, WhyChoose, Contact } from "../components";
 import NavbarV2 from "../components/Navbar&Sidebar/NavbarV2";
 import SliderFooter from "../components/SliderFooter";
+import { SliderHeadline } from "../components";
+
 const HomePage = () => {
   return (
     <main>
       <NavbarV2 />
       <SliderFooter />
+      <SliderHeadline />
       <TopSlider />
       <SampleProducts />
       <WhyChoose />
