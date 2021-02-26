f79c0ee47344853a42534e987e6078533adff4f2
BurakCaglar <burakcaglar@yandex.com>
2021-02-26 03:43:41 +0300

    component export updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,5 +2,16 @@ import { ReactComponent as Money } from "./money.svg";
 import { ReactComponent as Payments } from "./payments.svg";
 import { ReactComponent as Quality } from "./quality.svg";
 import { ReactComponent as Shipping } from "./shipping.svg";
+import { ReactComponent as FooterImg1 } from "./slider/footer-image-1.svg";
+import { ReactComponent as FooterImg2 } from "./slider/footer-image-2.svg";
+import { ReactComponent as FooterImg3 } from "./slider/footer-image-3.svg";
 
-export { Money, Payments, Quality, Shipping };
+export {
+  Money,
+  Payments,
+  Quality,
+  Shipping,
+  FooterImg1,
+  FooterImg2,
+  FooterImg3,
+};
