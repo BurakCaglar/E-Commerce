3101b42a69df3f4b8c32d80c4324b21a1af32e04
BurakCaglar <burakcaglar@yandex.com>
2021-02-26 20:39:07 +0300

    cart page design changed.

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { useCartContext } from "../context/useCartContext";
 import { Link } from "react-router-dom";
 import { Breadcrumb, CartContent } from "../components";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 
 const CartPage = () => {
   const { cart } = useCartContext();
@@ -17,12 +18,15 @@ const CartPage = () => {
           </Wrapper>
         </main>
       ) : (
-        <Wrapper className="page-100">
+        <Wrapper className="page-70">
           <div className="empty">
-            <h2>empty</h2>
-            <Link to="/products" className="btn">
-              Go To Products
-            </Link>
+            <h2>Empty Basket</h2>
+            <div className="go-to-products">
+              <Link to="/products" className="go-to-products-info">
+                <AiOutlineArrowLeft />
+                <p>Go To Products</p>
+              </Link>
+            </div>
           </div>
         </Wrapper>
       )}
@@ -31,11 +35,36 @@ const CartPage = () => {
 };
 
 const Wrapper = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   .empty {
+    margin-bottom: 10rem;
     text-align: center;
     h2 {
       margin-bottom: 1rem;
       text-transform: none;
+      font-size: 5rem;
+      font-weight: 300;
+    }
+    .go-to-products {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      .go-to-products-info {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+      p {
+        font-size: 2rem;
+        color: black;
+      }
+      svg {
+        font-size: 2rem;
+        color: black;
+        margin-right: 1rem;
+      }
     }
   }
 `;
